43d5ea8ce9f468c0102515edfa50c06dbe875822
vkrusen <victorkrusenstrahle@gmail.com>
2017-04-02 19:22:53 +0200

    Initial Commit

---
+++ CupOfMemes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CupOfMemes
+//
+//  Created by Victor Krusenstråhle on 2017-04-02.
+//  Copyright © 2017 Victor Krusenstråhle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
