4fc74adc3dab9a18fdb88e7ee8c70c13fecc9c9e
vkrusen <victorkrusenstrahle@gmail.com>
2018-07-13 22:39:06 +0300

    make it better

--- StreetUP/New Group/MyAPIClient.swift
+++ StreetUP/New Group/MyAPIClient.swift
@@ -6,13 +6,79 @@
 //  Copyright © 2016 Stripe. All rights reserved.
 //
 
+//
+//  BackendAPIAdapter.swift
+//  Standard Integration (Swift)
+//
+//  Created by Ben Guo on 4/15/16.
+//  Copyright © 2016 Stripe. All rights reserved.
+//
+
+import Foundation
+import Stripe
+import Alamofire
+
+class MyAPIClient: NSObject, STPEphemeralKeyProvider {
+    
+    static let sharedClient = MyAPIClient()
+    var baseURLString: String? = nil
+    var baseURL: URL {
+        if let urlString = self.baseURLString, let url = URL(string: urlString) {
+            return url
+        } else {
+            fatalError()
+        }
+    }
+    
+    func completeCharge(_ result: STPPaymentResult,
+                        amount: Int,
+                        shippingAddress: STPAddress?,
+                        shippingMethod: PKShippingMethod?,
+                        completion: @escaping STPErrorBlock) {
+        let url = self.baseURL.appendingPathComponent("charge")
+        var params: [String: Any] = [
+            "source": result.source.stripeID,
+            "amount": amount
+        ]
+        params["shipping"] = STPAddress.shippingInfoForCharge(with: shippingAddress, shippingMethod: shippingMethod)
+        Alamofire.request(url, method: .post, parameters: params)
+            .validate(statusCode: 200..<300)
+            .responseString { response in
+                switch response.result {
+                case .success:
+                    completion(nil)
+                case .failure(let error):
+                    completion(error)
+                }
+        }
+    }
+    
+    func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
+        let url = self.baseURL.appendingPathComponent("ephemeral_keys")
+        Alamofire.request(url, method: .post, parameters: [
+            "api_version": apiVersion,
+            ])
+            .validate(statusCode: 200..<300)
+            .responseJSON { responseJSON in
+                switch responseJSON.result {
+                case .success(let json):
+                    completion(json as? [String: AnyObject], nil)
+                case .failure(let error):
+                    completion(nil, error)
+                }
+        }
+    }
+    
+}
+
+/* - Firebase
 import Foundation
 import Stripe
 import Alamofire
 import Firebase
 
 class MyAPIClient: NSObject, STPEphemeralKeyProvider {
-
+    
     var customerID = "cus_DDVQ3OSLzdnf4X"
     var baseURLString: String? = nil
     
@@ -32,7 +98,8 @@ class MyAPIClient: NSObject, STPEphemeralKeyProvider {
                         completion: @escaping STPErrorBlock) {
         
         let UID = Auth.auth().currentUser?.uid
-        let url = self.baseURL.appendingPathComponent("/stripe_customers/\(UID!)/charges")
+        let id = arc4random()
+        let url = self.baseURL.appendingPathComponent("/stripe_customers/\(UID!)/charges/\(id)")
         print(url)
         var params: [String: Any] = [
             "source": result.source.stripeID,
@@ -40,6 +107,8 @@ class MyAPIClient: NSObject, STPEphemeralKeyProvider {
             "amount": amount
         ]
         params["shipping"] = STPAddress.shippingInfoForCharge(with: shippingAddress, shippingMethod: shippingMethod)
+        print(params)
+        
         Alamofire.request(url, method: .post, parameters: params)
             .validate(statusCode: 200..<300)
             .responseString { response in
@@ -154,4 +223,4 @@ class MyAPIClient: NSObject, STPEphemeralKeyProvider {
      }
      }*/
 
-}
+}*/
