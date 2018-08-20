2b1c80ef57cd0d2dbb32b97ad3bc0f09526b9067
vkrusen <victorkrusenstrahle@gmail.com>
2018-08-20 23:50:45 +0200

    0.0.222

--- StreetUP/New Group1/DetailViewController.swift
+++ StreetUP/New Group1/DetailViewController.swift
@@ -29,14 +29,20 @@
 import UIKit
 import AlamofireImage
 
-class DetailViewController: UIViewController {
+class DetailViewController: BaseViewController {
   
     @IBOutlet var imageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var breedLabel: UILabel!
     @IBOutlet var priceLabel: UILabel!
-    @IBOutlet var imageActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet var conditionLabel: UILabel!
+    @IBOutlet var sizeLabel: UILabel!
     @IBOutlet var BINButton: UIButton!
+    @IBOutlet var BidButton: UIButton!
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var sendOfferButton: UIButton!
+    
+    @IBOutlet var priceView: UIView!
+    @IBOutlet var conditionView: UIView!
+    @IBOutlet var sizeView: UIView!
     
     var item: Item!
     var buttonStatus = 0
@@ -52,31 +58,53 @@ class DetailViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    setupView(view: priceView, color: "A1F152", shadowColor: "417505")
+    setupView(view: conditionView, color: "52D1F1", shadowColor: "054D75")
+    setupView(view: sizeView, color: "529EF1", shadowColor: "054D75")
+    
+    setupGradient(item: BINButton, colors: [hexStringToUIColorWithAlpha(hex: "87D300", alpha: 1.0), hexStringToUIColorWithAlpha(hex: "35BA00", alpha: 1.0)], alpha: [1.0], locations: [0.0, 1.0], roundedCorners: true, cornerRadius: 7)
+    setupShadow(UIItem: BINButton, offsetX: -3, offsetY: 3, spread: 0, alpha: 1.0, HEXColor: "3FBD06")
+    BINButton.layer.cornerRadius = 7
+    setupGradient(item: BidButton, colors: [hexStringToUIColorWithAlpha(hex: "87D300", alpha: 1.0), hexStringToUIColorWithAlpha(hex: "35BA00", alpha: 1.0)], alpha: [1.0], locations: [0.0, 1.0], roundedCorners: true, cornerRadius: 7)
+    setupShadow(UIItem: BidButton, offsetX: -3, offsetY: 3, spread: 0, alpha: 1.0, HEXColor: "3FBD06")
+    BidButton.layer.cornerRadius = 7
+    
+    title = item.name
+    
     print(item)
     
-    imageActivityIndicator.startAnimating()
-    nameLabel.text = item.name
-    breedLabel.text = item.breed
+    setupImageView(imageView: imageView)
+    
+    conditionLabel.text = "DSWT"
+    sizeLabel.text = "M"
     priceLabel.text = NumberFormat.format(value: item.price)
     imageView.af_setImage(withURL: item.photoUrl) { [weak self] _ in
       guard let strongSelf = self else {
         return
       }
-      strongSelf.imageActivityIndicator.stopAnimating()
     }
   }
+    
+    // Functions
+    func setupImageView(imageView: UIImageView) {
+        let view = UIView(frame: imageView.frame)
+        let gradient = CAGradientLayer()
+            gradient.frame = view.frame
+            gradient.colors = [hexStringToUIColorWithAlpha(hex: "FFFFFF", alpha: 0.0).cgColor, hexStringToUIColorWithAlpha(hex: "FFFFFF", alpha: 1.0).cgColor]
+            gradient.locations = [0.2, 0.5]
+            view.layer.insertSublayer(gradient, at: 0)
+        
+        imageView.addSubview(view)
+        imageView.bringSubview(toFront: view)
+    }
+    
+    // Factories
+    func setupView(view: UIView, color: String, shadowColor: String) {
+        setupGradient(item: view, colors: [hexStringToUIColorWithAlpha(hex: color, alpha: 1.0), hexStringToUIColorWithAlpha(hex: color, alpha: 1.0)], alpha: [1.0], locations: [0.0    ,1.0], roundedCorners: true, cornerRadius: 7)
+        setupShadow(UIItem: view, offsetX: -3, offsetY: 3, spread: 0, alpha: 1.0, HEXColor: shadowColor)
+        view.layer.cornerRadius = 7
+    }
   
-  @IBAction func addToCartDidTap(_ sender: Any) {
-    CheckoutCart.shared.addItem(item)
-    if buttonStatus == 0 {
-        BINButton.setTitle("Betala?", for: .normal)
-        buttonStatus = 1
-    } else {
-        let storyboard = UIStoryboard(name: StoryboardNames.Main, bundle: nil)
-        guard let CheckoutViewController = storyboard.instantiateViewController(withIdentifier: ViewControllerIdentifiers.Checkout) as? CheckoutViewController else {
-            return
-        }
-        navigationController?.pushViewController(CheckoutViewController, animated: true)
+    @IBAction func dismiss(_ sender: Any) {
     }
-  }
 }
--- StreetUP/New Group1/PurchaseSuccessfulViewController.swift
+++ StreetUP/New Group1/PurchaseSuccessfulViewController.swift
@@ -11,9 +11,20 @@ import Stripe
 
 class PurchaseSuccessfulViewController: BaseViewController {
     
+    @IBOutlet var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Genomfört"
+        setupGradient(item: sendButton, colors: [hexStringToUIColorWithAlpha(hex: "87D300", alpha: 1.0), hexStringToUIColorWithAlpha(hex: "35BA00", alpha: 1.0)], alpha: [1.0], locations: [0.0    ,1.0], roundedCorners: true, cornerRadius: 7)
+        setupShadow(UIItem: sendButton, offsetX: -3, offsetY: 3, spread: 0, alpha: 1.0, HEXColor: "3FBD06")
+        sendButton.layer.cornerRadius = 7
+    }
+    
+    @IBAction func sendAction(_ sender: Any) {
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
     }
     
 }
